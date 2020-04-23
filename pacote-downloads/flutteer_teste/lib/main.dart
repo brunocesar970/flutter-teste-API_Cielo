a9f39c4fec36fd8741fa531eda3f9cca98e89ff7
caioVSR <caio.vitor.souza.reis@gmail.com>
2020-04-23 18:26:14 -0300

    alterado package da cielo

--- pacote-downloads/flutteer_teste/lib/main.dart
+++ pacote-downloads/flutteer_teste/lib/main.dart
@@ -1,72 +1,74 @@
-import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cielo/flutter_cielo.dart';
-import 'package:http/http.dart'as http;
-import 'dart:async';
+import 'package:cielo_ecommerce/cielo_ecommerce.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-const request = "https://api.cieloecommerce.cielo.com.br/";
-void main (){
+const baseURL = "https://api.cieloecommerce.cielo.com.br/";
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
- final nameController = TextEditingController();
+final nameController = TextEditingController();
 final numeroCartaoController = TextEditingController();
 final dataController = TextEditingController();
-final CVVController = TextEditingController();
+final cvvController = TextEditingController();
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
-//inicia objeto da api
   final CieloEcommerce cielo = CieloEcommerce(
-      environment: Environment.PRODUCTION, // ambiente de desenvolvimento
+      environment: Environment.production,
       merchant: Merchant(
         merchantId: "57be18b4-e44c-4a5d-9688-e360ccc9d27a",
         merchantKey: "mebOy0Q6U4OxVghgDoXCbfP1xcNVTH6q1uDmGikP",
       ));
 
+  bool loading = false;
 
-//Objeto de venda
-  Sale sale = Sale(
-      merchantOrderId: "24345", // id único de sua venda
-      customer: Customer( //objeto de dados do usuário
-          name: "BRUNO CESAR PINTO"
+  _makePayment() async {
+    print("Transação Simples");
+    print("Iniciando pagamento....");
+    //Objeto de venda
+    Sale sale = Sale(
+      merchantOrderId: "2020032601", // Numero de identificação do Pedido
+      customer: Customer(
+        name: "Comprador crédito simples", // Nome do Comprador
       ),
-      payment: Payment(    // objeto para de pagamento
-          type: TypePayment.creditCard, //tipo de pagamento
-          amount: 300, // valor da compra em centavos
-          installments: 1, // número de parcelas
-          softDescriptor: "Cielo", //descrição que aparecerá no extrato do usuário. Apenas 15 caracteres
-          creditCard: CreditCard( //objeto de Cartão de crédito
-            cardNumber: numeroCartaoController.text, //número do cartão
-            holder: nameController.text, //nome do usuário impresso no cartão
-            expirationDate: dataController.text, // data de expiração
-            securityCode: CVVController.text, // código de segurança
-            brand: "Master", // bandeira
-          )
-      )
-  );
+      payment: Payment(
+        type: TypePayment.creditCard, // Tipo do Meio de Pagamento
+        amount: 9, // Valor do Pedido (ser enviado em centavos)
+        installments: 1, // Número de Parcelas
+        softDescriptor:
+            "Mensagem", // Descrição que aparecerá no extrato do usuário. Apenas 15 caracteres
+        creditCard: CreditCard(
+          cardNumber: "4024007153763191", // Número do Cartão do Comprador
+          holder: 'Teste accept', // Nome do Comprador impresso no cartão
+          expirationDate: '08/2030', // Data de validade impresso no cartão
+          securityCode:
+              '123', // Código de segurança impresso no verso do cartão
+          brand: 'Visa', // Bandeira do cartão
+          saveCard: true,
+        ),
+      ),
+    );
 
-  CreditCard cart = CreditCard(
-    customerName: "BRUNO CESAR PINTO",
-    cardNumber: numeroCartaoController.text,
-    holder: nameController.text,
-    expirationDate: dataController.text,
-    brand: "Master",
-  );
+    try {
+      var response = await cielo.createSale(sale);
 
-
-
+      print('Payment accepted with paymentId: ${response.payment.paymentId}, \nTokenID: ${response.payment.tid}');
+    } on CieloException catch (e) {
+      print(e);
+      print(e.message);
+      print(e.errors[0].message);
+      print(e.errors[0].code);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,91 +86,76 @@ class _HomeState extends State<Home> {
               decoration: InputDecoration(
                 hintText: "Nº Cartão",
               ),
-            keyboardType: TextInputType.number,
+              keyboardType: TextInputType.number,
               controller: numeroCartaoController,
             ),
             SizedBox(height: 16.0),
             TextFormField(
-              decoration: InputDecoration(
-                  hintText: "Nome"
-              ),
-            controller: nameController,
+              decoration: InputDecoration(hintText: "Nome"),
+              controller: nameController,
             ),
-            SizedBox(height: 16.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "expiração data MM/AAAA"
-                  ),
-                    keyboardType: TextInputType.datetime,
-                  controller: dataController,
-                ),
             SizedBox(height: 16.0),
             TextFormField(
-              decoration: InputDecoration(
-                  hintText: "CVV"
-              ),
-                keyboardType: TextInputType.number,
-              controller: CVVController,
+              decoration: InputDecoration(hintText: "expiração data MM/AAAA"),
+              keyboardType: TextInputType.datetime,
+              controller: dataController,
             ),
-            SizedBox(height: 25.0,),
-            SizedBox(height: 44.0,
-            child: RaisedButton(
-              child: Text("Comprar",
-              style: TextStyle(fontSize: 18.0),
-              ),
-              color: Colors.blue,
-              onPressed: () async {
-
-                try{
-                  var response = await cielo.createSale(sale);
-                  print("numero paymentID" + response.payment.paymentId);
-                  http.Response response2 = await http.post(request);
-                  json.decode(response2.body);
-                  print(response2);
-
-                }
-                on CieloException catch(e){
-                  print(e.message);
-                  print(e.errors[0].message);
-                  print(e.errors[0].code);
-                }
-
-              },
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: "CVV"),
+              keyboardType: TextInputType.number,
+              controller: cvvController,
             ),
+            SizedBox(
+              height: 25.0,
             ),
-            SizedBox(height: 25.0,),
-            SizedBox(height: 44.0,
+            SizedBox(
+              height: 44.0,
               child: RaisedButton(
-                child: Text("Toquenizando o cartao",
+                child: loading ? CircularProgressIndicator(backgroundColor: Colors.white,) : Text(
+                  "Comprar",
                   style: TextStyle(fontSize: 18.0),
                 ),
                 color: Colors.blue,
                 onPressed: () async {
-                  print("numero cartao: ${numeroCartaoController.text}");
-                    print("TAMANHO DO CAMPO: ${numeroCartaoController.text.length}");
-                  try {
-                    var response = await cielo.tokenizeCard(cart);
-                    print(response.cardToken);
-                  } on CieloException catch(e){
-                    print(e.message);
-                    print(e.errors[0].message);
-                    print(e.errors[0].code);
-                  }
-
-
-
+                  setState(() {
+                    loading = true;
+                  });
+                  await _makePayment();
+                  toast();
+                   setState(() {
+                    loading = false;
+                  });
                 },
               ),
             ),
-
-
+            SizedBox(
+              height: 25.0,
+            ),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                child: Text(
+                  "Toquenizando o cartao",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                color: Colors.blue,
+                onPressed: null,
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
-
 }
 
-
+toast() => Fluttertoast.showToast(
+        msg: "Pagamento aprovado com sucesso!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.TOP,
+        timeInSecForIosWeb: 2,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
